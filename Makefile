1509aef4236be37f2d0846841c2d9f4bd4140bc2
Kengo Ide <kasumikagari@zeus.eonet.ne.jp>
2015-01-07 16:22:30 +0900

    OOP wannabe

--- Makefile
+++ Makefile
@@ -4,4 +4,8 @@ LIBS=sdl2 icu-uc
 all: $(BIN)
 
 $(BIN): sdl2im.c
-	gcc sdl2im.c -o $@ -std=c99 `pkg-config --cflags --libs $(LIBS)` -lSDL2_ttf
+	gcc sdl2im.c -g -o $@ -std=c99 `pkg-config --cflags --libs $(LIBS)` -lSDL2_ttf
+
+clean:
+	rm -f *.o
+	rm $(BIN)
--- sdl2im.c
+++ sdl2im.c
@@ -9,7 +9,27 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
+typedef void (*UIElementOp_Render)(void *this, SDL_Renderer *renderer);
+
+struct UIElementOperations {
+    UIElementOp_Render render;
+};
+
+struct UIElementHeader {
+    struct UIElementOperations *op;
+    struct UIElementHeader *kids;
+    struct UIElementHeader *siblings;
+};
+
+struct Screen {
+    struct UIElementHeader header;
+
+    SDL_Renderer *renderer;
+};
+
 struct TextField {
+    struct UIElementHeader header;
+
     SDL_Rect rect;
     int padding;
     TTF_Font* font;
@@ -23,55 +43,6 @@ struct TextField {
     size_t composition_bufsize;
     size_t composition_length;
 };
-
-static void
-text_field_update_text_info(struct TextField* field) {
-    TTF_SizeUTF8(field->font, field->text, &field->text_width, NULL);
-
-    SDL_Rect rect;
-    rect.x = field->rect.x + field->text_width;
-    rect.y = field->rect.y;
-    rect.w = 1;
-    rect.h = field->rect.h;
-    SDL_SetTextInputRect(&rect);
-}
-
-static void
-text_field_create(int x, int y, int width, int height,
-                  TTF_Font *font, struct TextField *field) {
-    field->rect = ((SDL_Rect){ x, y, width, height });
-    field->padding = 2;
-    if (!height) {
-        field->rect.h = TTF_FontHeight(font) + field->padding * 2;
-    }
-    field->font = font;
-    field->text_bufsize = 128;
-    field->text = calloc(field->text_bufsize, sizeof(char));
-    field->text_len = strlen(field->text);
-    field->text_width = 0;
-    field->composition_bufsize = 128;
-    field->composition = calloc(field->composition_bufsize, sizeof(char));
-    field->composition_length = 0;
-    text_field_update_text_info(field);
-}
-
-struct Screen {
-    SDL_Renderer *renderer;
-    TTF_Font *font;
-    struct TextField field;
-};
-
-static void
-dump_textediting(SDL_TextEditingEvent *e) {
-    printf("{timestamp=%d, \"%s\", start=%d, length=%d}\n",
-           e->timestamp, e->text, e->start, e->length);
-}
-
-static void
-exit_handler() {
-    TTF_Quit();
-    SDL_Quit();
-}
 
 static void
 draw_text(struct SDL_Renderer *renderer, SDL_Point *position,
@@ -92,54 +63,138 @@ draw_text(struct SDL_Renderer *renderer, SDL_Point *position,
 }
 
 static void
-redraw(const struct Screen *screen) {
-    // ** background **
-
-    SDL_SetRenderDrawColor(screen->renderer, 240, 240, 240, 255);
-    SDL_RenderClear(screen->renderer);
-
-    // ** text field **
-
+text_field_op_render(struct TextField *this, SDL_Renderer *renderer) {
     // border
-    SDL_SetRenderDrawColor(screen->renderer, 96, 96, 96, 255);
-    SDL_RenderDrawRect(screen->renderer, &screen->field.rect);
+    SDL_SetRenderDrawColor(renderer, 96, 96, 96, 255);
+    SDL_RenderDrawRect(renderer, &this->rect);
 
-    SDL_Point inner_pos = {screen->field.rect.x + screen->field.padding,
-                           screen->field.rect.y + screen->field.padding};
+    SDL_Color text_color = {0, 0, 0};
+    SDL_Point inner_pos = {this->rect.x + this->padding,
+                           this->rect.y + this->padding};
 
     // actual text
-    SDL_Color text_color = {0, 0, 0};
-    if (screen->field.text[0]) {
-        draw_text(screen->renderer, &inner_pos,
-                  screen->font, screen->field.text, text_color);
+    if (this->text[0]) {
+        draw_text(renderer, &inner_pos, this->font, this->text, text_color);
     }
 
-    int caretx = inner_pos.x + screen->field.text_width;
-
     // composition text
-    if (screen->field.composition[0]) {
-        int width;
-        TTF_SizeUTF8(screen->font, screen->field.composition, &width, NULL);
+    int composition_width = 0;
+    if (this->composition[0]) {
+        SDL_Point pos = {inner_pos.x + this->text_width, inner_pos.y};
+        draw_text(renderer, &pos, this->font,
+                  this->composition, text_color);
 
-        // text
-        SDL_Point pos = {inner_pos.x + screen->field.text_width, inner_pos.y};
-        draw_text(screen->renderer, &pos, screen->font,
-                  screen->field.composition, text_color);
+        TTF_SizeUTF8(this->font, this->composition, &composition_width, NULL);
 
-        // underline
-        int x = inner_pos.x + screen->field.text_width;
-        int h = inner_pos.y + TTF_FontHeight(screen->font);
-        SDL_RenderDrawLine(screen->renderer, x, h, x + width, h);
-
-        caretx += width;
+        int x = inner_pos.x + this->text_width;
+        int h = inner_pos.y + TTF_FontHeight(this->font);
+        SDL_RenderDrawLine(renderer, x, h, x + composition_width, h);
     }
 
     // caret
-    int bottom = screen->field.rect.y + screen->field.rect.h;
-    SDL_RenderDrawLine(screen->renderer,
-                       caretx, inner_pos.y,
-                       caretx, bottom - screen->field.padding);
+    int x = inner_pos.x + this->text_width + composition_width;
+    int bottom = this->rect.y + this->rect.h;
+    SDL_RenderDrawLine(renderer, x, inner_pos.y, x, bottom - this->padding);
+}
 
+static void
+text_field_update_text_info(struct TextField *this) {
+    TTF_SizeUTF8(this->font, this->text, &this->text_width, NULL);
+
+    SDL_Rect rect;
+    rect.x = this->rect.x + this->text_width;
+    rect.y = this->rect.y;
+    rect.w = 1;
+    rect.h = this->rect.h;
+    SDL_SetTextInputRect(&rect);
+}
+
+static struct TextField *
+text_field_create(struct UIElementHeader *parent, int x, int y,
+                  int width, int height, TTF_Font *font) {
+    static struct UIElementOperations op = {
+        .render = (UIElementOp_Render)text_field_op_render
+    };
+
+    struct TextField *this = malloc(sizeof(struct TextField));
+    if (!this) {
+        return NULL;
+    }
+    this->header = ((struct UIElementHeader){&op, NULL, parent->kids});
+    parent->kids = this;
+
+    this->rect = ((SDL_Rect){ x, y, width, height });
+    this->padding = 2;
+    if (!height) {
+        this->rect.h = TTF_FontHeight(font) + this->padding * 2;
+    }
+    this->font = font;
+    this->text_bufsize = 128;
+    this->text = calloc(this->text_bufsize, sizeof(char));
+    this->text_len = strlen(this->text);
+    this->text_width = 0;
+    this->composition_bufsize = 128;
+    this->composition = calloc(this->composition_bufsize, sizeof(char));
+    this->composition_length = 0;
+    text_field_update_text_info(this);
+    return this;
+}
+
+static void
+text_field_destroy(struct TextField *this) {
+    free(this->text);
+    free(this->composition);
+    free(this);
+}
+
+static void
+screen_op_render(struct Screen *this, SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255);
+    SDL_RenderClear(renderer);
+}
+
+static struct Screen *
+screen_create(SDL_Renderer *renderer) {
+    static struct UIElementOperations op = {
+        .render = (UIElementOp_Render)screen_op_render
+    };
+
+    struct Screen *this = malloc(sizeof(struct Screen));
+    if (!this) {
+        return NULL;
+    }
+    this->header = ((struct UIElementHeader){&op, NULL, NULL});
+    this->renderer = renderer;
+    return this;
+}
+
+static void
+screen_destroy(struct Screen *this) {
+    free(this);
+}
+
+static void
+exit_handler() {
+    TTF_Quit();
+    SDL_Quit();
+}
+
+static void
+render(struct UIElementHeader *element, SDL_Renderer *renderer) {
+    struct UIElementHeader *sibling;
+
+    element->op->render(element, renderer);
+    if (element->kids) {
+        render(element->kids, renderer);
+    }
+    while ((sibling = element->siblings)) {
+        render(sibling, renderer);
+    }
+}
+
+static void
+screen_render(struct Screen *screen) {
+    render((struct UIElementHeader *)screen, screen->renderer);
     SDL_RenderPresent(screen->renderer);
 }
 
@@ -151,24 +206,26 @@ main() {
     TTF_Init();
     atexit(exit_handler);
 
-    SDL_Window *window = SDL_CreateWindow("SDL2 Japanese Input", SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED, 640, 240,
-                                          SDL_WINDOW_OPENGL);
+    SDL_Window *window =
+        SDL_CreateWindow("SDL2 Japanese Input",
+                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                         640, 240, SDL_WINDOW_OPENGL);
     if (!window) {
         fputs("FAIL: Failed to create the window.", stderr);
         exit(EXIT_FAILURE);
     }
 
-    struct Screen screen;
-    screen.renderer = SDL_CreateRenderer(window, -1, 0);
-    screen.font = TTF_OpenFont(TTF_PATH, 20);
-    if (!screen.font) {
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    TTF_Font *font = TTF_OpenFont(TTF_PATH, 20);
+    if (!font) {
         fprintf(stderr, "FAIL: Couldn't open %s.", TTF_PATH);
         exit(EXIT_FAILURE);
     }
-
-    text_field_create(20, 20, 600, 0, screen.font, &screen.field);
-    redraw(&screen);
+    struct Screen *screen = screen_create(renderer);
+    struct TextField *field =
+        text_field_create((struct UIElementHeader *)screen,
+                          20, 20, 600, 0, font);
+    screen_render(screen);
 
     SDL_StartTextInput();
     for (;;) {
@@ -185,36 +242,37 @@ main() {
             size_t len = strlen(e.edit.text);
             if (!e.edit.start) {
                 assert(len < 127);
-                strcpy(screen.field.composition, e.edit.text);
-                screen.field.composition_length = len;
+                strcpy(field->composition, e.edit.text);
+                field->composition_length = len;
             } else {
-                assert(screen.field.composition_length + len < 127);
-                strcpy(screen.field.composition + screen.field.composition_length,
+                assert(field->composition_length + len < 127);
+                strcpy(field->composition + field->composition_length,
                        e.edit.text);
-                screen.field.composition_length += len;
+                field->composition_length += len;
             }
-            dump_textediting(&e.edit);
-            redraw(&screen);
+            printf("{timestamp=%d, \"%s\", start=%d, length=%d}\n",
+                   e.edit.timestamp, e.edit.text, e.edit.start, e.edit.length);
+            screen_render(screen);
             break;
         }
         case SDL_TEXTINPUT: {
-            if (screen.field.composition[0]) {
-                screen.field.composition[0] = '\0';
-                screen.field.composition_length = 0;
+            if (field->composition[0]) {
+                field->composition[0] = '\0';
+                field->composition_length = 0;
             }
-            strcat(screen.field.text, e.text.text);
-            screen.field.text_len = strlen(screen.field.text);
-            text_field_update_text_info(&screen.field);
-            redraw(&screen);
+            strcat(field->text, e.text.text);
+            field->text_len = strlen(field->text);
+            text_field_update_text_info(field);
+            screen_render(screen);
             break;
         }
         case SDL_KEYUP:
             switch (e.key.keysym.sym) {
             case SDLK_BACKSPACE:
-                U8_BACK_1(screen.field.text, 0, screen.field.text_len);
-                screen.field.text[screen.field.text_len] = '\0';
-                text_field_update_text_info(&screen.field);
-                redraw(&screen);
+                U8_BACK_1(field->text, 0, field->text_len);
+                field->text[field->text_len] = '\0';
+                text_field_update_text_info(field);
+                screen_render(screen);
                 break;
             }
             break;
@@ -223,8 +281,10 @@ main() {
 out:
     SDL_StopTextInput();
 
-    TTF_CloseFont(screen.font);
-    SDL_DestroyRenderer(screen.renderer);
+    text_field_destroy(field);
+    screen_destroy(screen);
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     exit(EXIT_SUCCESS);
 }
